a892ec75aeaceb56de0cbe8a1e0d9629b3dbfc1f
yolooo1 <136896510+yolooo1@users.noreply.github.com>
2024-05-08 01:12:47 +0800

    提交文件了

--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'y76aoafez$2!45yrm-0f!k2$!*ji@s+h+u2%bt$6bkrd2=cz#s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['*']
